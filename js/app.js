73f2928e97bd72f41067b0042361cc9f81a6742d
jrdurrcodes <biycoachn@yahoo.com>
2024-11-19 10:00:35 -0600

    corrected errors on agetester

--- js/app.js
+++ js/app.js
@@ -17,74 +17,88 @@ const submitBtn = document.getElementById('submitBtn')
 class Tester {
     constructor() {
         this.displayText = document.getElementById('displayText')
-        // this.dateOfBirth = document.getElementById('dateOfBirth')
     }
 
-    init () {
+    init() {
         // console.log('clicked')
         this.getDateOfBirth()
-    }   
+    }
 
     getDateOfBirth() {
-    const dateOfBirth = document.getElementById('dateOfBirth').value
-        // 1979-11-12
+        const dateOfBirth = document.getElementById('dateOfBirth').value
 
         // str.slice(startPos, limit)
-        //parseInt(str)
-        const birthYear  = parseInt(dateOfBirth.slice(0, 4))
+        // parseinit(str)
+        const birthYear = parseInt(dateOfBirth.slice(0, 4))
         const birthMonth = parseInt(dateOfBirth.slice(5, 7))
-        const birthDate  = parseInt(dateOfBirth.slice(8))  
-        
+        const birthDate = parseInt(dateOfBirth.slice(8))
+
         const today = new Date()
         const year = today.getFullYear()
-        const month = today.getMonth() + 1
+        const month = today.getMonth() +1
         const date = today.getDate()
-        // console.log(date)
-        
-        
-        // console.log(birthDate, date)
-        //run tests
+
+        // run tests
         const yearTest = year - birthYear
         const monthTest = month - birthMonth
         const dateTest = date - birthDate
-    
-        let access
+
+        // let access
 
         let granted = 'Access Granted'
-        let denied = ' Access Denied'
+        let denied = 'Access Denied'
 
-        const accessGranted = (access)=> {
-            if  (access === 'granted')
+        // function accessGranted
+        const accessGranted = (access) => {
+            if(access === 'granted') {
                 this.displayText.classList.remove('denied')
                 this.displayText.classList.add('granted')
                 this.displayText.innerText = granted
-            }else if (access === 'denied')
+            } else if (access === 'denied') {
                 this.displayText.classList.remove('granted')
                 this.displayText.classList.add('denied')
                 this.displayText.innerText = denied
+            }
         }
-        // function accessGranted
-}
 
         const validate = document.getElementById('checkbox')
-        
+
         if (validate.checked) {
-            //test dates
-            if(yearTest > 21)
+            console.log(validate.checked)
+            // test dates
+            if (yearTest > 21) {
                 accessGranted('granted')
-        }else if (yearTest ===21) {
-            if(monthTest >= 0 )  {
-                accessGranted('granted')
+            } else if (yearTest === 21) {
+                if (monthTest > 0) {
+                    accessGranted('granted')
+                } else if (monthTest === 0) {
+                    if (dateTest >= 0 ) {
+                        accessGranted('granted')
+                    } else {
+                        accessGranted('denied')
+                    }
+                } else {
+                    accessGranted('denied')
+                }
+            } else {
+                accessGranted('denied')
             }
-        }else { 
-            accessGranted()
+        } else {
+            this.displayText.innerText = 'Please check box!'
         }
-        }
-        }
+    }
+}
+
 
 const action = new Tester()
 
 submitBtn.addEventListener('click', (e)=> {
     e.preventDefault()
     action.init()
+})
+
+
+
+submitBtn.addEventListener('click', (e)=> {
+    e.preventDefault()
 })
